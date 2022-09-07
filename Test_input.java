ca9bd09030837a18141e39b33fccd00312acdd6f
Haorui Xu <a1781040@student.adelaide.edu.au>
2022-09-07 20:06:55 +0930

    add test

---
+++ Test_input.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+public class Test_input {
+    @Test
+    public void testinput(){
+        vaildInput Vi=new vaildInput("ab");
+        int IN=1+1;
+        assertEquals(IN,VI);
+    }
+}
